9c629f6767b0e6f8fd54cf4317d93496ef688799
Mike McTighe <m.mctighe10@gmail.com>
2019-09-28 01:15:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,6 +30,6 @@ end
 #    course = Course.new
 #    course.title = post.css("h2").text
 #    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+#    course.description = post.css("p").text
   end
 end
