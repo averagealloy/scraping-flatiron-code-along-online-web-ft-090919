94666290665648eb2cabac8efd76375b9c219caa
Mike McTighe <m.mctighe10@gmail.com>
2019-09-28 01:16:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,6 +30,6 @@ def make_courses
     course = Course.new
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
-#    course.description = post.css("p").text
-#  end
+    course.description = post.css("p").text
+  end
 #end
